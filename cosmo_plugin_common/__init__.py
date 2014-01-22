0309ef02e7cc6dd0780da9f76c4946d99e8526a9
Ilya Sher <ilya.sher@coding-knight.com>
2014-01-22 18:52:28 +0200

    Better config error reporting // setup.py (untested)

--- cosmo_plugin_common/__init__.py
+++ cosmo_plugin_common/__init__.py
@@ -5,7 +5,16 @@ class Config(object):
 	def get(self):
 		which = self.__class__.which
 		env_name = which.upper() + '_CONFIG_PATH'
-		config_path = os.getenv(env_name, os.path.expanduser('~/' + which + '_config.json'))
-		with open(config_path) as f:
-			cfg = json.loads(f.read())
+		default_location_tpl = '~/' + which + '_config.json'
+		default_location = os.path.expanduser(default_location_tpl)
+		config_path = os.getenv(env_name, default_location)
+		try:
+			with open(config_path) as f:
+				cfg = json.loads(f.read())
+		except IOError:
+			raise RuntimeError(
+				"Failed to read {0} configuration from file '{1}'."
+				"The configuration is looked up in {2}. If defined, environment variable "
+				"{3} overrides that location.".format(
+					which, config_path, default_location_tpl, env_name))
 		return cfg
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+setuptools.setup(
+    zip_safe=True,
+    name='cosmo-plugin-common',
+    version='0.1',
+    author='Ilya Sher',
+    author_email='ilya.sher@coding-knight.com',
+    packages=['cosmo_plugin_common'],
+    license='LICENSE',
+    description='Common code for Cosmo plugins',
+)
